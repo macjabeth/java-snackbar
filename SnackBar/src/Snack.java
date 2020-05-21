e306ebe1139e39b5558ff4ba663914941989831e
macjabeth <macbethjonathan@gmail.com>
2020-05-21 15:23:39 -0400

    add snack class

---
+++ SnackBar/src/Snack.java
@@ -0,0 +1,70 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Snack {
+    private static final AtomicInteger maxId = new AtomicInteger(0);
+    private final int id;
+    private String name;
+    private int quantity;
+    private double cost;
+    private int vendingMachineId;
+
+    public Snack() {
+        id = maxId.incrementAndGet();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Snack setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Snack setCost(double cost) {
+        this.cost = cost;
+        return this;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Snack setVendingMachineId(int vendingMachineId) {
+        this.vendingMachineId = vendingMachineId;
+        return this;
+    }
+
+    public int getVendingMachineId() {
+        return vendingMachineId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Snack addQuantity(int quantity) {
+        this.quantity += quantity;
+        System.out.println(String.format("%s stock increased by %d.", name, quantity));
+        return this;
+    }
+
+    public boolean buy(int quantity) {
+        boolean inStock = this.quantity >= quantity;
+        if (inStock) this.quantity -= quantity;
+        return inStock;
+    }
+
+    public double getTotalCost(int quantity) {
+        return cost * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Quantity of snack %d (%s) is %d.", id, name, quantity);
+    }
+}
